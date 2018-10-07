0d55570a00208e41f45a7cb4ae8b171968906d23
Poulpitore <Poulpitore@hotmail.com>
2018-10-07 15:30:37 +0200

    a0.0.0

--- load.py
+++
@@ -1,6 +0,0 @@
-'''
-load the images etc...
-'''
-from tools import *
-icon = tools.load_convert("Assets\Image\Icon.png", 32, False, 1)
-homesprite = tools.load_convert("Assets\Image\HomeSprite.png", 65, True, 1)
